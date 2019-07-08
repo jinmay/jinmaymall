3f2346e1d30efc5e464826de2f69ae99713c2b7b
jinmay <britzegs@gmail.com>
2019-07-08 10:29:42 +0900

    user 모델 admin 구현

--- user/admin.py
+++ user/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+admin.site.unregister(Group)
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'is_staff', 'is_active', )
+class UserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_superuser', 'is_active', )
     fieldsets = (
         (('Personal Info'), {'fields': ('username', 'email', 'password')}),
         (('Permissions'), {
@@ -12,5 +15,13 @@ class UserAdmin(admin.ModelAdmin):
         }),
         (('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
     ordering = ('username',)
     filter_horizontal = ('groups', 'user_permissions',)
+    search_fields = ('username', 'email')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
