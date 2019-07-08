a2f70a1e95b6acca939a93fdf2bad1507bf670a7
jinmay <britzegs@gmail.com>
2019-07-08 17:19:46 +0900

    accounts app에서 회원가입 했을때 login 페이지로 리다이렉트

--- accounts/views.py
+++ accounts/views.py
@@ -8,7 +8,7 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            return redirect('login')
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {
