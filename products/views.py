9c4f909a8a2ca5df3f311caafccd7d7ef67efafb
jinmay <britzegs@gmail.com>
2019-07-08 17:51:13 +0900

    admin 등록 & ProductListView 생성

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Category, Product
 
-# Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/product_list.html'
+
+product_list = ProductListView.as_view()
