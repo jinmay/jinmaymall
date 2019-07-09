6f48fb7968b11f3ba9a473157f18e7558e0460fe
jinmay <britzegs@gmail.com>
2019-07-09 13:09:29 +0900

    Product app - detail 기능 간단 구현
    view 함수 기능 구현
    나중에 템플릿 작업 해야함

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
-from .models import Category, Product
+from .models import Product
 
 
 class ProductListView(ListView):
@@ -8,3 +8,10 @@ class ProductListView(ListView):
     template_name = 'products/product_list.html'
 
 product_list = ProductListView.as_view()
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/product_detail.html', {
+        'product': product,
+    })
