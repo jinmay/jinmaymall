bee0ad21cf4f4229e820d39657e8d53fdead9da4
jinmay <britzegs@gmail.com>
2019-07-12 14:33:34 +0900

    Fix typo & products app의 urls.py에 appname 추가
    Product 모델의 get_absolute_url의 잘못된 namespace 수정

--- products/models.py
+++ products/models.py
@@ -30,7 +30,7 @@ class Product(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('product:product_detail', kwargs={'product_id': self.id})
+        return reverse('products:product_detail', kwargs={'product_id': self.id})
 
     @property
     def like_count(self):
