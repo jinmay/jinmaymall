e58bee9a3981b70c217161d5d124dae81a48ff6d
jinmay <britzegs@gmail.com>
2019-07-14 15:02:58 +0900

    root 경로를 products의 list 페이지로 redirect
    HTML 파일 정리

--- jinmaymall/urls.py
+++ jinmaymall/urls.py
@@ -1,7 +1,12 @@
+from django.shortcuts import redirect
 from django.contrib import admin
 from django.urls import path, include
 
+def index(request):
+    return redirect('products:product_list')
+
 urlpatterns = [
+    path('', index, name='root'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('products/', include('products.urls')),
