0bdb0321e43590599d7532702bd515fbc5a917e7
jinmay <britzegs@gmail.com>
2019-07-17 17:36:55 +0900

    media 파일을 위한 설정 추가

--- jinmaymall/settings/base.py
+++ jinmaymall/settings/base.py
@@ -140,5 +140,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'jinmaymall', 'static'),
 ]
 
+# Media files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # accounts app
 LOGIN_REDIRECT_URL = '/'
