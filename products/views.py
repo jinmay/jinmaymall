51a02323c1ead0728261dccbc82226ce15404de5
jinmay <britzegs@gmail.com>
2019-07-19 16:59:34 +0900

    좋아요 기능 비동기 구현 완료
    비로그인 상태 -> 로그인 리다이렉트
    로그인 상태 -> 비동기 좋아요 기능 수행

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.list import ListView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import AnonymousUser
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -29,7 +30,10 @@ def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     category_list = Category.objects.all()
     product_total_count = Product.objects.count()
-    like = product.get_like(request.user)
+    try:
+        like = product.get_like(request.user)
+    except:
+        like = None
 
     return render(request, 'products/product_detail.html', {
         'product': product,
@@ -38,12 +42,21 @@ def product_detail(request, product_id):
         'like': like,
     })
 
+# 유저 객체가 AnonymousUser클래스의 인스턴스인지 확인 후
+# AnonymousUser의 객체 -> 로그인 페이지로 redirect
+# 로그인한 유저 객체 -> 비동기로 좋아요 기능을 수행
 @api_view(['POST'])
-@login_required
 def toggle_like(request, product_id):
     user = request.user
+    if isinstance(user, AnonymousUser):
+        data = {
+            'is_login': False
+        }
+        return Response(data=data, status=status.HTTP_200_OK)
+
     product = get_object_or_404(Product, pk=product_id)
     data = {
+        'is_login': True,
         'is_existed': None
     }
     try:
