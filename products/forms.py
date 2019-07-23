b2ae15cf76a799079ae2843c9a33a09dce80a515
jinmay <britzegs@gmail.com>
2019-07-23 16:37:59 +0900

    상품 업로드시 사진에 대한 유효성 검사 루틴 추가
    기존에 있던 두 개 이상 검사하는 것에 0개 일때도 검사하는 루틴 추가

--- products/forms.py
+++ products/forms.py
@@ -12,3 +12,6 @@ class ProductImageInlineFormset(forms.models.BaseInlineFormSet):
                 pass
         if count > 1:
             raise forms.ValidationError('대표 사진이 두 장 이상입니다. 하나의 사진만 대표 사진으로 지정해주세요.')
+        if count == 0:
+            raise forms.ValidationError('대표 사진이 없습니다. 하나의 사진으르 대표사진으로 지정해주세요.')
+            
