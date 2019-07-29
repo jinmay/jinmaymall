6f12509831eed35ba17e93158b0910d9c781949c
jinmay <britzegs@gmail.com>
2019-07-29 11:36:00 +0900

    accounts 앱의 모든 url을 project 가장 최상위로 변경

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,9 +3,5 @@ from products import views as products_views
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('mypage/', views.mypage, name='mypage'),
-    path('wishlist/', products_views.like_list, name='wishlist'),
+    
 ]
--- jinmaymall/urls.py
+++ jinmaymall/urls.py
@@ -3,15 +3,25 @@ from django.conf.urls.static import static
 from django.shortcuts import redirect
 from django.contrib import admin
 from django.urls import path, include
+from accounts import views as accounts_views
+from products import views as products_views
 
 def index(request):
     return redirect('products:product_list')
 
 urlpatterns = [
     path('', index, name='root'),
-    path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+
+    path('admin/', admin.site.urls),
+
     path('accounts/', include('accounts.urls')),
+    path('signup/', accounts_views.signup, name='signup'),
+    path('login/', accounts_views.login, name='login'),
+    path('logout/', accounts_views.logout, name='logout'),
+    path('mypage/', accounts_views.mypage, name='mypage'),
+    path('wishlist/', products_views.like_list, name='wishlist'),
+
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
 ]
