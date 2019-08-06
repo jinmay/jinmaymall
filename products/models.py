5b0fd530d40189ee478ee924d5c57586d27773f8
jinmay <britzegs@gmail.com>
2019-08-06 22:41:33 +0900

    Review 모델의 rating 필드에 최소값과 최대값을 검증할 수 있도록 validators 추가

--- products/models.py
+++ products/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth import get_user_model
@@ -107,7 +108,7 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     content = models.TextField()
-    rating = models.PositiveSmallIntegerField()
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
